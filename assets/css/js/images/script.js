7d34b6ff9e8c00279ca2386743f36788d52c6626
Matt <mjd014@live.co.uk>
2022-05-21 11:50:07 +0000

    modified js file.

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -2,11 +2,11 @@
 // get the button element and add event listeners to them.
 
 document.addEventListener('DOMContentLoaded', function() {
-    let buttons = getElementsByTagName('button');
+    let buttons = document.getElementsByTagName('button');
     for (let button of buttons) {
         button.addEventListener('click', function() {
             if (this.getAttribute('data-type') === 'submit') {
-                alert('You clicked submit!');
+                alert('You clicked Submit!');
             } else {
                 let gameType = this.getAttribute('data-type');
                 alert(`You clicked ${gameType}`);
