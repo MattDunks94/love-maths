0f654625951a3f6a3f1ccc7bef414ea375b9aa08
Matt <mjd014@live.co.uk>
2022-05-22 11:37:55 +0000

    attempted to add division game

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract'){
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === 'division') {
+        displayMultiplyQuestion (num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division'];
     } else {
         alert (`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
